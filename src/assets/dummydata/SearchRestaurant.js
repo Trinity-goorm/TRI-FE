255b295ad7d0dc245c609decfd46a9797532eb99
chaeheonjeong <cogjs3210@naver.com>
2025-02-17 13:24:05 +0900

    TRI-54 feat: 식당 리스트 화면

---
+++ src/assets/dummydata/SearchRestaurant.js
@@ -0,0 +1,43 @@
+const SearchRestaurant = [
+  {
+    id: 1,
+    name: "스시이루카",
+    imgUrl: [
+      "https://ugc-images.catchtable.co.kr/catchtable/shopinfo/sE0BlVNIno5xhCO793TeazA/m/c467a3fc86c948e29895c8c8d5f228af",
+      "https://ugc-images.catchtable.co.kr/catchtable/shopinfo/sE0BlVNIno5xhCO793TeazA/m/c467a3fc86c948e29895c8c8d5f228af",
+    ],
+    category: "중식",
+    location: "잠실",
+    rating: 4.8,
+    operatingHour: "월-토 : 11:00 ~ 23:00",
+    averagePrice: "1-2만원",
+    isLike: true,
+  },
+  {
+    id: 2,
+    name: "스시이루카",
+    imgUrl: [
+      "https://ugc-images.catchtable.co.kr/catchtable/shopinfo/sE0BlVNIno5xhCO793TeazA/m/c467a3fc86c948e29895c8c8d5f228af",
+      "https://ugc-images.catchtable.co.kr/catchtable/shopinfo/sE0BlVNIno5xhCO793TeazA/m/c467a3fc86c948e29895c8c8d5f228af",
+      "https://ugc-images.catchtable.co.kr/catchtable/shopinfo/sE0BlVNIno5xhCO793TeazA/m/c467a3fc86c948e29895c8c8d5f228af",
+    ],
+    category: "중식",
+    location: "잠실",
+    rating: 4.8,
+    operatingHour: "월-토 : 11:00 ~ 23:00",
+    averagePrice: "1-2만원",
+    isLike: false,
+  },
+  {
+    id: 3,
+    name: "스시이루카",
+    imgUrl: null,
+    category: "중식",
+    location: "잠실",
+    rating: 4.8,
+    operatingHour: "월-토 : 11:00 ~ 23:00",
+    averagePrice: "1-2만원",
+    isLike: false,
+  },
+];
+export default SearchRestaurant;
