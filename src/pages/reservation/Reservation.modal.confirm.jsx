a0c1fbaf8f195f055ba48bf19896571cfbaeb7f3
chaeheonjeong <cogjs3210@naver.com>
2025-02-25 01:00:40 +0900

    temp

--- src/pages/reservation/Reservation.modal.confirm.jsx
+++ src/pages/reservation/Reservation.modal.confirm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import * as style from "./style/Reservation.modal.confirm.js";
 import ReservationButton from "../../components/button/ReservationButton.jsx";
-import Modal from "../../../components/Modal/Modal" // CI/CD중 경로 오류가 있어서 고쳤습니다. (24-21:50. by toby)
+import Modal from "../../components/Modal/Modal"; // CI/CD중 경로 오류가 있어서 고쳤습니다. (24-21:50. by toby)
 //API
 import postPreoccupy from "../../api/reservation/post/PostPreoccupy.js";
 import { useEffect } from "react";
@@ -133,69 +133,3 @@ const ReservationConfirm = () => {
   );
 };
 export default ReservationConfirm;
-=======
-
-    useEffect(() => {
-        if (reservationData.reservationId) {
-            onMovetoPayment();
-        }
-    }, [reservationData.reservationId]);
-
-
-    return (
-        <>
-            <style.Background>
-                <style.TotalContainer>
-                    <style.TitleContainer>
-                        예약금 안내
-                    </style.TitleContainer>
-                    <style.TicketInfoContainer>
-                        <style.FirstInfoContainer>
-                            인원에 상관 없이 티켓이 사용됩니다!
-                        </style.FirstInfoContainer>
-                        <style.SecondInfoContainer>
-                            티켓 10개 차감
-                        </style.SecondInfoContainer>
-                        <style.ThirdInfoContainer>
-                            💵 사용 티켓 금액은 매장에서 결제 시 반환해 드려요!
-                        </style.ThirdInfoContainer>
-                    </style.TicketInfoContainer>
-                    <style.RefundInfoContainer>
-                        <style.RefundInfoTitle>
-                            환불 정책
-                        </style.RefundInfoTitle>
-                        <style.RefundInfoContent>
-                            - 노쇼 시: 사용 티켓 환불 불가 <br/>
-                            - 당일 취소: 사용 티켓 환불 불가 <br/>
-                            - 3일 전까지 취소: 사용 티켓 100% 환불
-                        </style.RefundInfoContent>
-                    </style.RefundInfoContainer>
-                    <style.ButtonContainer>
-                        <style.ButtonEachContainer onClick={onCancel}>
-                            <ReservationButton
-                                name={"취소"} width={"200px"} height={"60px"} backcolor={"white"} namecolor={"lightgray"}
-                                border={"1px solid lightgray"}/>
-                        </style.ButtonEachContainer>
-                        <style.ButtonEachContainer onClick={handleSubmit}>
-                            <ReservationButton name={"확인"} width={"200px"} height={"60px"} backcolor={"#FF6868"}
-                                               namecolor={"white"}/>
-                        </style.ButtonEachContainer>
-                    </style.ButtonContainer>
-                </style.TotalContainer>
-            </style.Background>
-            {isTicketLack && (
-                <Modal
-                    isOpen={isTicketLack}
-                    onClose={()=>setIsTicketLack(false)}
-                    onCancel={()=>setIsTicketLack(false)}
-                    message={"티켓이 부족합니다!"}
-                    positionx={"center"}
-                    positiony={"center"}
-                    borderRadius={"20px"}
-                    innerMessage={"💵 티켓을 구매해 주세요! 💵"}
-                />
-            )}
-        </>
-    )
-}
-export default ReservationConfirm
