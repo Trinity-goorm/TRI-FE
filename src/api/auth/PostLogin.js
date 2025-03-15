b106ea4e9ec72a8394f7bb367c5e9e536011298c
chaeheonjeong <cogjs3210@naver.com>
2025-03-15 20:37:40 +0900

    fix: 로그인, 로그아웃, 리이슈 api uri 수정

--- src/api/auth/PostLogin.js
+++ src/api/auth/PostLogin.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const PostLogin = async (authCode) => {
   try {
-    const response = await axios.post(`${BASE_URL}/login`, {
+    const response = await axios.post(`${BASE_URL}/api/login`, {
       authCode,
     });
 
     return response;
   } catch (error) {
-    console.error("로그인 실패:", error.response?.data || error.message);
-    throw new Error("로그인 실패");
+    console.error('로그인 실패:', error.response?.data || error.message);
+    throw new Error('로그인 실패');
   }
 };
 
--- src/api/auth/PostLogout.js
+++ src/api/auth/PostLogout.js
@@ -1,18 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const PostLogout = async (refreshToken) => {
-  console.log("PostLogout", refreshToken);
   try {
-    const response = await axios.post(`${BASE_URL}/logout`, null, {
+    const response = await axios.post(`${BASE_URL}/api/logout`, null, {
       headers: {
         refresh: refreshToken,
       },
     });
     return response.data;
   } catch (error) {
-    console.error("로그아웃 실패", error);
+    console.error('로그아웃 실패', error);
     throw error;
   }
 };
